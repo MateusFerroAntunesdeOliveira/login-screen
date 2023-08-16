7f3250dac91784946466a6134758f06bd0c2a7ef
tasi.pasin <tasi.pasin96@gmail.com>
2023-08-16 19:17:29 -0300

    - Altera página inicial; - Adiciona verificação de quantidade de logins inválidos.

--- login.js
+++ login.js
@@ -2,32 +2,46 @@ const usernameInput = document.getElementById('usernameInput');
 const passwordInput = document.getElementById('passwordInput');
 const togglePassword = document.getElementById('togglePassword');
 
-document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('login-form');
-    loginForm.addEventListener('submit', async function(event) {
-        event.preventDefault();
+let errorCounter = 0;
 
-        const username = usernameInput.value;
-        const password = passwordInput.value;
-        
-        try {
-            const response = await fetch("users.json");
-            const data = await response.json();
-            const users = data.users.find(user => user.username === username && user.password === password);
+let data = {};
 
-            if (users) {
+document.addEventListener('DOMContentLoaded', async function() {
+    const response = await fetch("users.json");
+    data = await response.json();
+});
+
+// Evento executado ao clicar no botão de Login
+loginBtn.addEventListener('click', async function(event) {
+    event.preventDefault();
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    try {
+        const user = data.users.find(user => user.username === username);
+        if (user) {
+            if (user.blocked) {
+                alert("Muitas tentativas erradas. Conta Bloqueada");
+            }
+            else if (user.password === password) {
                 document.body.style.backgroundColor = "#57ff56";
                 alert("Bem vindo, usuário");
             } else {
-                document.body.style.backgroundColor = "#ff5556";
-                alert("Login ou senha incorreto, tente novamente mais tarde");
+                if (errorCounter++ < 5) {
+                    console.log(errorCounter);
+                    document.body.style.backgroundColor = "#ff5556";
+                    alert("Login ou senha incorreto, tente novamente mais tarde");
+                } else {
+                    data.users.find(user => user.username === username).blocked = true;
+                    alert("Muitas tentativas erradas. Conta Bloqueada");
+                }
             }
-        } catch (error) {
-            console.log("Erro: ", error);
         }
-    });
+    } catch (error) {
+        console.log("Erro: ", error);
+    }
 });
 
+// Evento ao clicar no botão de mostrar/ocultar a senha
 togglePassword.addEventListener('click', () => {
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
