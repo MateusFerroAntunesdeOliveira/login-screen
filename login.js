eab62e22ab25a6097b52a29781c58f59d8111157
tasi.pasin <tasi.pasin96@gmail.com>
2023-08-17 18:40:37 -0300

    - Remove o alerta do navegador; - Adicionada área de mensagem de retorno do login.

--- login.js
+++ login.js
@@ -2,17 +2,17 @@ const usernameInput = document.getElementById('usernameInput');
 const passwordInput = document.getElementById('passwordInput');
 const togglePassword = document.getElementById('togglePassword');
 
-let errorCounter = 0;
+let errorCounter = 1;
 
 let data = {};
 
-document.addEventListener('DOMContentLoaded', async function() {
+document.addEventListener('DOMContentLoaded', async function () {
     const response = await fetch("users.json");
     data = await response.json();
 });
 
 // Evento executado ao clicar no botão de Login
-loginBtn.addEventListener('click', async function(event) {
+loginBtn.addEventListener('click', function (event) {
     event.preventDefault();
     const username = usernameInput.value;
     const password = passwordInput.value;
@@ -20,21 +20,29 @@ loginBtn.addEventListener('click', async function(event) {
         const user = data.users.find(user => user.username === username);
         if (user) {
             if (user.blocked) {
-                alert("Muitas tentativas erradas. Conta Bloqueada");
+                enableErrorArea("Muitas tentativas erradas. Conta Bloqueada");
             }
             else if (user.password === password) {
                 document.body.style.backgroundColor = "#57ff56";
                 alert("Bem vindo, usuário");
+                document.getElementById('errorMessageDiv').style.display = "none";
+                errorCounter = 0;
             } else {
                 if (errorCounter++ < 5) {
                     console.log(errorCounter);
                     document.body.style.backgroundColor = "#ff5556";
-                    alert("Login ou senha incorreto, tente novamente mais tarde");
+                    var message = "Login ou senha incorreto, tente novamente mais tarde.";
+                    if (errorCounter > 1) {
+                        message = message.concat(" Tentativas restantes ").concat(5 - errorCounter + 1);
+                    }
+                    enableErrorArea(message);
                 } else {
                     data.users.find(user => user.username === username).blocked = true;
-                    alert("Muitas tentativas erradas. Conta Bloqueada");
+                    enableErrorArea("Muitas tentativas erradas. Conta Bloqueada");
                 }
             }
+        } else {
+            enableErrorArea("Usuário não encontrado.");
         }
     } catch (error) {
         console.log("Erro: ", error);
@@ -51,3 +59,8 @@ togglePassword.addEventListener('click', () => {
         togglePassword.textContent = 'Mostrar';
     }
 });
+
+function enableErrorArea(message) {
+    document.getElementById('errorMessageDiv').style.display = "block";
+    document.getElementById('errorMessage').innerText = message;
+}
